8d58d8a904a79cc99d0edb7523d2e98eacba8f6f
GeorgeSamuelOdhiambo <51238253+GeorgeSamuelOdhiambo@users.noreply.github.com>
2021-06-27 21:21:11 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Info from "./Note";
+import Note from "./Note";
 
 const App = () => {
   return(
    <div>
       <Header/>
-      <Info/>
-      <Info/>
-      <Info/>
-      <Info/>
+      <Note/>
+      <Note/>
+      <Note/>
+      <Note/>
     <Footer/>
    </div>
   );
